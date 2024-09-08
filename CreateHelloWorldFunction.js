c3becf83bba45b3f44642d3ab6f70693ab58b7c0
achyutem <achyutem.ad@gmail.com>
2024-09-08 17:07:35 +0530

    day 1

---
+++ CreateHelloWorldFunction.js
@@ -0,0 +1,8 @@
+/* https://leetcode.com/problems/create-hello-world-function/?envType=study-plan-v2&envId=30-days-of-javascript */
+
+const createHelloWorld = () => {
+    
+    return function() {
+        return 'Hello World'
+    }
+};
