dba5957f61eafe6530feb2fe45854630c65eda2c
achyutem <achyutem.ad@gmail.com>
2024-09-09 10:21:28 +0530

    ApplyTransformOverEachElement

---
+++ ApplyTransformOverEachElement.js
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/apply-transform-over-each-element-in-array */
+
+
+const map = (arr, fn) => {
+result = []
+
+for (let i=0; i < arr.length; i++){
+    result.push(fn(arr[i], i))
+}
+
+return result
+}
