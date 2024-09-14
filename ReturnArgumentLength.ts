8b29b7ba803a0a74b2472fa56ed51102b9a6d5d1
achyutem <achyutem.ad@gmail.com>
2024-09-14 19:27:14 +0530

    return arguement length

---
+++ ReturnArgumentLength.ts
@@ -0,0 +1,7 @@
+/* https://leetcode.com/problems/return-length-of-arguments-passed */
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+const argumentsLength = (...args: JSONValue[]): number => {
+    return args.length
+};
