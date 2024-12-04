1e91e143a321c284d5e6f951998ff21b80e58abf
Achyutem <111311052+Achyutem@users.noreply.github.com>
2024-12-04 17:24:26 +0530

    Intercal Cancellation

---
+++ IntervalCancellation.ts
@@ -0,0 +1,10 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    fn(...args)
+    let intervalId = setInterval(() => fn(...args), t)
+
+    let cancelFn = () => clearInterval(intervalId)
+    return cancelFn
+};
