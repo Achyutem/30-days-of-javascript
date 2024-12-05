92a08bc37989e2b52243c8be9af091ff7357c22d
Achyutem <111311052+Achyutem@users.noreply.github.com>
2024-12-05 18:08:28 +0530

    use setTimeout instead of setInterval

--- PromiseTimeLimit.ts
+++ PromiseTimeLimit.ts
@@ -3,7 +3,7 @@ type Fn = (...args: any[]) => Promise<any>;
 function timeLimit(fn: Fn, t: number): Fn {
     return async function (...args: any[]) {
         return new Promise((resolve, reject) => {
-            const timer = setInterval(() => {
+            const timer = setTimeout(() => {
                 reject('Time Limit Exceeded')
             }, t)
 
